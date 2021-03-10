ab1cec275f18fcf414c2d0ea74c027490eec17c3
Azamat Duisheev <azamat27071998@gmail.com>
2021-03-10 16:34:57 +0600

    Initial commit

---
+++ src/megacom/Main.java
@@ -0,0 +1,18 @@
+package megacom;
+
+import megacom.models.MyList;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyList myList = new MyList();
+
+        myList.sortNumbers();
+        myList.outputSortedList();
+
+    }
+}
